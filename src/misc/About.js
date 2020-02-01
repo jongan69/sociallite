dd762e4dfa55ef7d40d21a1249856cae4898e1c5
Jose Placeres <jplaceres3559@floridapoly.edu>
2020-02-01 03:17:45 -0500

    changed the about page

--- src/misc/About.js
+++ src/misc/About.js
@@ -23,15 +23,9 @@ function About() {
         About Us
       </Typography>
       <Typography paragraph>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry&#39;s standard dummy text
-        ever since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Social Lite helps people improve their social skills through
+        recommendations and challenges. Once the user completes the task, they
+        are rewarded.
       </Typography>
     </div>
   );
